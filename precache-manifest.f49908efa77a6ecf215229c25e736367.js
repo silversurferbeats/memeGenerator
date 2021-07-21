0321d352004d57a4f0f440196266ce355c605500
silversurferbeats <gabrielsuarez89@gmail.com>
2021-07-21 12:34:29 -0300

    Updates

---
+++ precache-manifest.f49908efa77a6ecf215229c25e736367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f3523c462c71c7b4ad1cc7fa9c2f3d",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "c2a6761634cdea4d8946",
+    "url": "/memegenerator/static/css/main.e83559e6.chunk.css"
+  },
+  {
+    "revision": "9ec10a36aea9381d4c45",
+    "url": "/memegenerator/static/js/2.a8297fcc.chunk.js"
+  },
+  {
+    "revision": "c2a6761634cdea4d8946",
+    "url": "/memegenerator/static/js/main.2fe0a090.chunk.js"
+  },
+  {
+    "revision": "e40966f54bc993d0e999",
+    "url": "/memegenerator/static/js/runtime~main.71a76f7c.js"
+  }
+]);
