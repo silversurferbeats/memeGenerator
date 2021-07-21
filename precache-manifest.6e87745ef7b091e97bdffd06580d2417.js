b292c70d2d31973fabddcd1b6d9e02e591ed57f4
silversurferbeats <gabrielsuarez89@gmail.com>
2021-07-21 12:41:48 -0300

    Updates

---
+++ precache-manifest.6e87745ef7b091e97bdffd06580d2417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020eb8a8e136209ee25a78c9446f3acd",
+    "url": "/memeGenerator/index.html"
+  },
+  {
+    "revision": "5b4e0eba82ca0e53bab2",
+    "url": "/memeGenerator/static/css/main.e83559e6.chunk.css"
+  },
+  {
+    "revision": "9ec10a36aea9381d4c45",
+    "url": "/memeGenerator/static/js/2.a8297fcc.chunk.js"
+  },
+  {
+    "revision": "5b4e0eba82ca0e53bab2",
+    "url": "/memeGenerator/static/js/main.c18706bf.chunk.js"
+  },
+  {
+    "revision": "5131287b336b685cdfe4",
+    "url": "/memeGenerator/static/js/runtime~main.7d2c4351.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/memegenerator/precache-manifest.f49908efa77a6ecf215229c25e736367.js"
+  "/memeGenerator/precache-manifest.6e87745ef7b091e97bdffd06580d2417.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memegenerator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memeGenerator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
