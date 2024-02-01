c4a947b08d39d87081320f562d6308dd600e9e02
silversurferbeats <gabrielsuarez89@gmail.com>
2024-02-01 17:03:25 +0100

    feat(Meme): corrected styled to responsive

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Meme = ({ template, onClick }) => {
   return (
     <img
-      style={{ width: 300 }}
+      style={{ width: 300, maxHeight: "20rem"  }}
       src={template.url}
       alt={template.name}
       onClick={onClick}
